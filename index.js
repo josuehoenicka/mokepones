6be089e1c548af6e85af5f898b7c754656fa2589
Josué Hoenicka <josuee.vzla@gmail.com>
2023-05-05 12:39:10 -0300

    add pc option / add fight

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 let player = 0;
-let pc = 2;
+let pc = 0;
+let  = document.querySelector('p')
 
 function chooseItem() {
     player = prompt('Elige: 1 para piedra, 2 para papel, 3 para tijera');
+    // PLAYER OPTIONS
     if(player == 1){
-        alert('Elegiste piedra');
+        console.log('Tú: ✊');
     } else if(player == 2){
-        alert('Elegiste papel');
+        console.log('Tú: ✋');
     }else if (player == 3){
-        alert('Elegiste tijera');
+        console.log('Tú: ✌');
     } else {
-        alert('No elegiste nada');
+        console.log('No elegiste nada');
+    }
+    // PC OPTIONS
+    pc = prompt('Elige: 1 para piedra, 2 para papel, 3 para tijera');
+    if(pc == 1){
+        console.log('PC: ✊');
+    } else if(pc == 2){
+        console.log('PC: ✋');
+    }else if (pc == 3){
+        console.log('PC: ✌');
+    } else {
+        console.log('No elegiste nada');
+    }
+    // FIGHT
+    if(pc == player){
+        console.log('EMPATE ➖')
+    } else if(player == 1 && pc == 3){
+        console.log('GANASTE ✅')
+    } else if(player == 2 && pc == 1){
+        console.log('GANASTE ✅')
+    } else if(player == 3 && pc == 2){
+        console.log('GANASTE ✅')
+    } else {
+        console.log('PERDISTE ❌')
     }
 }
 
