3ead520ba5dde52d8d1d79890292db5a2078ae7f
Josué Hoenicka <josuee.vzla@gmail.com>
2023-05-15 11:22:26 -0300

    add runGame function / improve selectRottwaimer function

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-function selectRottwaimer() {
-    console.log('GOOD SELECTION!')
+function runGame() {
+    let btnRottwaimer = document.querySelector('#btn-rottwaimer');
+    btnRottwaimer.addEventListener('click', selectRottwaimer)
 }
 
-let btnRottwaimer = document.querySelector('#btn-rottwaimer');
-btnRottwaimer.addEventListener('click', selectRottwaimer)
+function selectRottwaimer() {
+    let hoenix = document.querySelector('#hoenix');   
+    let chaina = document.querySelector('#chaina');
+    let kaison = document.querySelector('#kaison');
+    if(hoenix.checked == true){
+        alert('You selected Hoenix');
+    } else if (chaina.checked == true) {
+        alert('You selected Chaina');
+    } else if (kaison.checked == true) {
+        alert('You selected Kaison');
+    } else {
+        alert('You must select a rottwaimer');
+    }
+}
+
+window.addEventListener('load', runGame)
