9fd23bfd6dbda0441ac4d9b4747eff3b89eb1c37
Josué Hoenicka <josuee.vzla@gmail.com>
2023-05-15 14:24:04 -0300

    logic to choose rottwaimer

--- index.js
+++ index.js
@@ -7,15 +7,33 @@ function selectRottwaimer() {
     let hoenix = document.querySelector('#hoenix');   
     let chaina = document.querySelector('#chaina');
     let kaison = document.querySelector('#kaison');
+    let playerName = document.querySelector('#rottwaimer-player');
     if(hoenix.checked == true){
-        alert('You selected Hoenix');
+        playerName.innerHTML = 'Hoenix'
     } else if (chaina.checked == true) {
-        alert('You selected Chaina');
+        playerName.innerHTML = 'Chaina'
     } else if (kaison.checked == true) {
-        alert('You selected Kaison');
+        playerName.innerHTML = 'Kaison'
     } else {
         alert('You must select a rottwaimer');
     }
+    selectEnemy()
+}
+
+function selectEnemy() {
+    let enemyRandom = numRamdon(1,3);
+    let playerComputer = document.querySelector('#rottwaimer-computer');
+    if(enemyRandom == 1) {
+        playerComputer.innerHTML = 'Hoenix';
+    } else if (enemyRandom == 2) {
+        playerComputer.innerHTML = 'Chaina';
+    } else {
+        playerComputer.innerHTML = 'Kaison';
+    }
+}
+
+function numRamdon(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 window.addEventListener('load', runGame)
